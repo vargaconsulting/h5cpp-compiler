ece80df66de3f5157a217885b0d6e34a11ae8782
steven varga <steven@vargaconsulting.ca>
2026-05-24 21:37:11 +0000

    [#34]:svarga:backend, MessagePack descriptor emitter with msgpack::attribute support

--- src/h5cpp.cpp
+++ src/h5cpp.cpp
@@ -18,6 +18,7 @@
 #include "producer_h5.hpp"
 #include "consumer.hpp"
 #include "h5_attr_translator.hpp"
+#include "consumer_msgpack.hpp"
 
 clang::ast_matchers::StatementMatcher h5templateMatcher = clang::ast_matchers::callExpr( clang::ast_matchers::allOf(
 	clang::ast_matchers::hasDescendant( clang::ast_matchers::declRefExpr( clang::ast_matchers::to( clang::ast_matchers::varDecl().bind("variableDecl")  ) ) ),
@@ -120,11 +121,12 @@ int main(int argc, const char **argv) {
 				llvm::errs() << "h5cpp-compiler: --format json not yet implemented\n";
 				rc = 1;
 				break;
-			case OutputFormat::msgpack:
-				llvm::errs() << "h5cpp-compiler: --format msgpack not yet implemented\n";
-				rc = 1;
+			case OutputFormat::msgpack: {
+				MsgpackTemplateCallback callback(work_path);
+				Finder.addMatcher(h5templateMatcher, &callback);
+				rc = Tool.run(clang::tooling::newFrontendActionFactory(&Finder).get());
 				break;
-			case OutputFormat::cbor:
+			}
 				llvm::errs() << "h5cpp-compiler: --format cbor not yet implemented\n";
 				rc = 1;
 				break;
