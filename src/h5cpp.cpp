8b6e39312e2493108f3fa750b8f9291aa4f52993
steven varga <steven@vargaconsulting.ca>
2026-05-24 21:51:06 +0000

    [#36]:svarga:backend, BSON descriptor emitter with bson::attribute support

--- src/h5cpp.cpp
+++ src/h5cpp.cpp
@@ -17,6 +17,7 @@
 
 #include "producer_h5.hpp"
 #include "consumer.hpp"
+#include "consumer_bson.hpp"
 #include "h5_attr_translator.hpp"
 
 clang::ast_matchers::StatementMatcher h5templateMatcher = clang::ast_matchers::callExpr( clang::ast_matchers::allOf(
@@ -128,10 +129,12 @@ int main(int argc, const char **argv) {
 				llvm::errs() << "h5cpp-compiler: --format cbor not yet implemented\n";
 				rc = 1;
 				break;
-			case OutputFormat::bson:
-				llvm::errs() << "h5cpp-compiler: --format bson not yet implemented\n";
-				rc = 1;
+			case OutputFormat::bson: {
+				BsonTemplateCallback callback(work_path);
+				Finder.addMatcher(h5templateMatcher, &callback);
+				rc = Tool.run(clang::tooling::newFrontendActionFactory(&Finder).get());
 				break;
+			}
 			case OutputFormat::avro:
 				llvm::errs() << "h5cpp-compiler: --format avro not yet implemented\n";
 				rc = 1;
